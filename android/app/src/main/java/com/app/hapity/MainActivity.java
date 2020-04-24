f436897d162035ce713acc3fc9343774ee7733f3
Egenie Next <egenie.apple@gmail.com>
2020-04-24 18:25:39 +0500

    update hapity android package name

--- android/app/src/main/java/com/app/hapity/MainActivity.java
+++ android/app/src/main/java/com/app/hapity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.hapityapp;
+package com.app.hapity;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/app/hapity/MainApplication.java
+++ android/app/src/main/java/com/app/hapity/MainApplication.java
@@ -1,4 +1,4 @@
-package com.hapityapp;
+package com.app.hapity;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.hapityapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.hapity.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
